bc97f85ea6a4b53d7fef91d1da157b51793dafc7
alvaroex <102678507+alvaroex@users.noreply.github.com>
2022-04-25 11:00:56 +0700

    Update logomaker.py

--- SiestaRobot/modules/logomaker.py
+++ SiestaRobot/modules/logomaker.py
@@ -197,7 +197,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Siesta Robot](https://t.me/Siestaxbot)",
+            caption="Made By [Alvaroxrobot](https://t.me/alvarooxrobot)",
             force_document=False,
         )
         os.remove(flnme)
