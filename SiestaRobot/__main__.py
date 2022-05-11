cd35953095e24dda510782e3d9179f5c74326ddf
alvaroex <102678507+alvaroex@users.noreply.github.com>
2022-05-11 07:07:24 +0700

    Update __main__.py

--- SiestaRobot/__main__.py
+++ SiestaRobot/__main__.py
@@ -217,7 +217,7 @@ def start(update: Update, context: CallbackContext):
                         ],
                         [
                             InlineKeyboardButton(
-                                text=gs(chat.id, "add_bot_to_group_button"), url="t.me/varooxrobot?startgroup=new"),
+                                text=gs(chat.id, "add_bot_to_group_button"), url="t.me/vaboutyoubot?startgroup=new"),
                         ]
                     ]
                 ),
